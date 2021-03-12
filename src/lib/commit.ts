8fc0ff2591e3add48f4f839e53c8b9ac6c91f1f2
Bob van Hesse <bob.van.hesse@governance.com>
2021-03-12 13:23:51 +0100

    Backport bug fix concerning start time of stories

--- src/lib/commit.ts
+++ src/lib/commit.ts
@@ -1,11 +1,13 @@
-import { Commit } from 'gitlog';
+import { Commit as GitLogCommit } from 'gitlog';
 import { default as moment } from 'moment';
 import { curryN } from 'ramda';
 
 import { GitToTempoConfig } from './config';
 import { Story } from './story';
 
-export const DATE_FORMAT_GIT: Readonly<string> = 'YYYY-MM-DD HH:mm:ss ZZ';
+export type Commit = Pick<GitLogCommit, 'committerDate' | 'rawBody'>;
+
+export const DATE_FORMAT_GIT_COMMITTERDATE: string = 'ddd MMM D HH:mm:ss YYYY Z';
 
 export const commitsToStories = curryN(2, (config: GitToTempoConfig, commits: Commit[]): Story[] => {
   return commits
@@ -17,9 +19,9 @@ export const commitsToStories = curryN(2, (config: GitToTempoConfig, commits: Co
         comment: commit.rawBody.replace(storyRegEx, ''),
         originTaskId: (commit.rawBody.match(storyRegEx) as RegExpMatchArray)[1],
         period: {
-          end: moment(commit.authorDate, DATE_FORMAT_GIT),
+          end: moment(commit.committerDate, DATE_FORMAT_GIT_COMMITTERDATE),
           start: commitIndex > 0
-            ? moment(commits[commitIndex - 1].authorDate, DATE_FORMAT_GIT)
+            ? moment(commits[commitIndex - 1].committerDate, DATE_FORMAT_GIT_COMMITTERDATE)
             : moment(0, 'x')
         },
       };
@@ -30,6 +32,6 @@ export const getStoryRegEx = (config: GitToTempoConfig): RegExp => {
   return new RegExp(`^\\s*(${config.prefix}-\\d+):?\\s+`);
 };
 
-export const isFormattedCommit = curryN(2, (config: GitToTempoConfig, commit: Commit): boolean => {
+export const isFormattedCommit = curryN(2, (config: GitToTempoConfig, commit: Pick<GitLogCommit, 'rawBody'>): boolean => {
   return commit.rawBody.match(getStoryRegEx(config)) instanceof Array;
 });
